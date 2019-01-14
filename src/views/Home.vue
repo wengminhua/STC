74642fbab1fc317748468f10c57c23d7af15aa37
unknown <wengminhua@live.com>
2019-01-14 22:01:38 +0800

    Add top bar menu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,14 +4,46 @@
       text-xs-center
       wrap
     >
-      <section class="top_banner" style="background-image: url(/imgs/home_top_banner_bg.jpg);">
+      <section
+        class="top-banner"
+        style="background-image: url(/imgs/home_top_banner_bg.jpg);"
+      >
+        <div class="topbar">
+          <span
+            class="topbar-logo"
+          >
+            STC
+          </span>
+          <div
+            class="topbar-menu"
+          >
+            <v-tabs
+              v-model="active"
+              dark
+              slider-color="white">
+              <v-tab
+                v-for="n in 3"
+                :key="n"
+                ripple
+              >
+                Item {{ n }}
+              </v-tab>
+            </v-tabs>
+          </div>
+        </div>
       </section>
     </v-layout>
   </v-container>
 </template>
 
 <script>
-
+  export default {
+    data () {
+      return {
+        active: null
+      }
+    },
+  }
 </script>
 
 <style>
