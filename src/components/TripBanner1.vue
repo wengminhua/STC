c23df06926f4e1f28be384594d3cc3c000d4f972
unknown <wengminhua@live.com>
2019-01-27 17:46:00 +0800

    Add pagination of viewpoint list

--- src/components/TripBanner1.vue
+++ src/components/TripBanner1.vue
@@ -1,38 +1,28 @@
 <template>
   <section
-    class="jdjs"
+    class="tsxl"
   >
     <p class="sub-title">
-      想去有名的地方
+      想要一些酷的地方
     </p>
     <p class="main-title">
-      景点介绍
+      热门目的地，特色线路
     </p>
     <v-layout style="margin-top: 40px">
      <v-flex>
        <v-container fluid grid-list-xs style="max-width: 1000px">
          <v-layout row wrap>
            <v-flex
-            v-for="viewpoint in viewpoint_list"
-            :key="viewpoint.pic"
+            v-for="line in line_list"
+            :key="line.pic"
             d-flex
             xs12 md4
             style="padding: 15px"
            >
-             <v-card flat>
-              <v-img
-                :src="viewpoint.pic"
-              >
-              </v-img>
-              <v-card-title
-                class="card-title"
-              >
-                <div>
-                  <div class="card-name" style="margin-bottom: 5px">{{ viewpoint.name }}</div>
-                  <div class="card-location">{{ viewpoint.location }}</div>
-                </div>
-              </v-card-title>
-            </v-card>
+             <v-img
+              :src="line.pic"
+            >
+            </v-img>
           </v-flex>
          </v-layout>
        </v-container>
@@ -45,36 +35,15 @@
 export default {
   data() {
     return {
-      viewpoint_list: [
+      line_list: [
         {
-          pic: '/imgs/trip_viewpoint_1.jpg',
-          name: '莫斯科红场',
-          location: '莫斯科'
+          pic: '/imgs/trip_line_1.jpg',
         },
         {
-          pic: '/imgs/trip_viewpoint_2.jpg',
-          name: '弗拉基米尔',
-          location: '莫斯科'
+          pic: '/imgs/trip_line_2.jpg',
         },
         {
-          pic: '/imgs/trip_viewpoint_3.jpg',
-          name: '苏兹达里镇',
-          location: '莫斯科'
-        },
-        {
-          pic: '/imgs/trip_viewpoint_4.jpg',
-          name: '谢尔盖耶夫镇',
-          location: '莫斯科'
-        },
-        {
-          pic: '/imgs/trip_viewpoint_5.jpg',
-          name: '阿芙乐尔号巡洋舰',
-          location: '莫斯科'
-        },
-        {
-          pic: '/imgs/trip_viewpoint_6.jpg',
-          name: '新年展览 NEW YEAR COUNTRY',
-          location: '莫斯科'
+          pic: '/imgs/trip_line_3.jpg',
         }
       ]
     }
@@ -83,37 +52,22 @@ export default {
 </script>
 
 <style>
-.jdjs {
+.tsxl {
   padding: 50px;
   position: relative;
   min-width: 720px;
   width: 100%;
+  background-color: #F7F7F7;
 }
 
-.jdjs .sub-title {
+.tsxl .sub-title {
   color: #999;
   font-size: 14px;
 }
 
-.jdjs .main-title {
+.tsxl .main-title {
   font-size: 30px;
   margin-top: 20px;
   font-weight: bold;
-}
-
-.jdjs .card-name {
-  font-size: 18px;
-  text-align: left;
-  font-weight: bold;
-}
-
-.jdjs .card-location {
-  color: #999;
-  text-align: left;
-}
-
-.jdjs .card-title {
-  padding-left: 0px;
-  padding-right: 0px;
 }
 </style>
--- src/components/TripBanner2.vue
+++ src/components/TripBanner2.vue
@@ -1,33 +1,62 @@
 <template>
   <section
-    class="tsxl"
+    class="jdjs"
   >
     <p class="sub-title">
-      想要一些酷的地方
+      想去有名的地方
     </p>
     <p class="main-title">
-      热门目的地，特色线路
+      景点介绍
     </p>
+    <v-tabs
+      v-model="currentCountry"
+      light
+      slider-color="grey"
+      centered>
+      <v-tab
+        v-for="country in country_list"
+        :key="country.country"
+        ripple
+      >
+        {{ country.country }}
+      </v-tab>
+    </v-tabs>
     <v-layout style="margin-top: 40px">
      <v-flex>
        <v-container fluid grid-list-xs style="max-width: 1000px">
          <v-layout row wrap>
            <v-flex
-            v-for="line in line_list"
-            :key="line.pic"
+            v-for="viewpoint in currentViewpointList"
+            :key="viewpoint.pic"
             d-flex
             xs12 md4
             style="padding: 15px"
            >
-             <v-img
-              :src="line.pic"
-            >
-            </v-img>
+             <v-card flat>
+              <v-img
+                :src="viewpoint.pic"
+              >
+              </v-img>
+              <v-card-title
+                class="card-title"
+              >
+                <div>
+                  <div class="card-name" style="margin-bottom: 5px">{{ viewpoint.name }}</div>
+                  <div class="card-location">{{ viewpoint.location }}</div>
+                </div>
+              </v-card-title>
+            </v-card>
           </v-flex>
          </v-layout>
        </v-container>
      </v-flex>
    </v-layout>
+   <v-pagination
+    v-model="page"
+    :length="viewpointLength"
+    circle
+   >
+   </v-pagination>
   </section>
 </template>
 
@@ -35,39 +64,167 @@
 export default {
   data() {
     return {
-      line_list: [
+      currentCountry: 0,
+      viewpoint_list: [],
+      page: 1,
+      pageSize: 6,
+      country_list: [
         {
-          pic: '/imgs/trip_line_1.jpg',
+          country: '俄罗斯',
+          viewpoint_list: [
+            {
+              pic: '/imgs/trip_viewpoint_1.jpg',
+              name: '莫斯科红场',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_2.jpg',
+              name: '弗拉基米尔',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_3.jpg',
+              name: '苏兹达里镇',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_4.jpg',
+              name: '谢尔盖耶夫镇',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_5.jpg',
+              name: '阿芙乐尔号巡洋舰',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_6.jpg',
+              name: '新年展览 NEW YEAR COUNTRY',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_4.jpg',
+              name: '谢尔盖耶夫镇',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_5.jpg',
+              name: '阿芙乐尔号巡洋舰',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_6.jpg',
+              name: '新年展览 NEW YEAR COUNTRY',
+              location: '莫斯科'
+            }
+          ]
         },
         {
-          pic: '/imgs/trip_line_2.jpg',
-        },
-        {
-          pic: '/imgs/trip_line_3.jpg',
+          country: '玻利维亚',
+          viewpoint_list: [
+            {
+              pic: '/imgs/trip_viewpoint_1.jpg',
+              name: '莫斯科红场',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_2.jpg',
+              name: '弗拉基米尔',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_3.jpg',
+              name: '苏兹达里镇',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_4.jpg',
+              name: '谢尔盖耶夫镇',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_5.jpg',
+              name: '阿芙乐尔号巡洋舰',
+              location: '莫斯科'
+            },
+            {
+              pic: '/imgs/trip_viewpoint_6.jpg',
+              name: '新年展览 NEW YEAR COUNTRY',
+              location: '莫斯科'
+            }
+          ]
         }
       ]
+    }
+  },
+  watch: {
+    currentCountry(val, oldVal) {
+      this.page = 1;
+    }
+  },
+  computed: {
+    viewpointLength() {
+      let length = this.country_list[this.currentCountry].viewpoint_list.length;
+      if(length % this.pageSize > 0) {
+        return parseInt(length / this.pageSize) + 1;
+      } else {
+        return length / this.pageSize;
+      }
+    },
+    currentViewpointList() {
+      let viewpointList = this.country_list[this.currentCountry].viewpoint_list;
+      let start = (this.page - 1) * this.pageSize;
+      let end = this.page * this.pageSize;
+      if(viewpointList.length < end) {
+        end = viewpointList.length;
+      }
+      let result = [];
+      for(let idx=start; idx<end; idx++) {
+        result.push(viewpointList[idx]);
+      }
+      return result;
     }
   }
 }
 </script>
 
 <style>
-.tsxl {
+.jdjs {
   padding: 50px;
   position: relative;
   min-width: 720px;
   width: 100%;
-  background-color: #F7F7F7;
 }
 
-.tsxl .sub-title {
+.jdjs .sub-title {
   color: #999;
   font-size: 14px;
 }
 
-.tsxl .main-title {
+.jdjs .main-title {
   font-size: 30px;
   margin-top: 20px;
   font-weight: bold;
+}
+
+.jdjs .card-name {
+  font-size: 18px;
+  text-align: left;
+  font-weight: bold;
+}
+
+.jdjs .card-location {
+  color: #999;
+  text-align: left;
+}
+
+.jdjs .card-title {
+  padding-left: 0px;
+  padding-right: 0px;
+}
+
+.primary {
+    background-color: #1867c0!important;
+    border-color: #1867c0!important;
 }
 </style>
